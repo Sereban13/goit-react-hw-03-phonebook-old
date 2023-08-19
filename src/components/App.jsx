3d337b3aee26a3a765f99e3d3eaf5277d42f9191
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-08-19 18:39:06 +0200

    json package changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ export class App extends Component {
   };
 
   changeNameFilter = newName => {
-    console.log(newName);
+    // console.log(newName);
     this.setState({
       filter: newName,
     });
